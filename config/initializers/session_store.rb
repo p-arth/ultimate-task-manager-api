cccb540fcfda7f6c8022f1b8e4f837d4e4c82d30
Pedro Arthur <pedro.art.vel@gmail.com>
2020-07-22 15:49:46 -0300

    Finished session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@ if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, key: "_ultimate-task-manager", domain: "ultimate-task-manager-api.herokuapp.com"
   else
     Rails.application.config.session_store :cookie_store, key: "_ultimate-task-manager"
-  end
+end
