91706248aef375af242856c7c1328456ee075832
Pedro Arthur <pedro.art.vel@gmail.com>
2020-07-22 15:48:10 -0300

    logged in and log out methods, and current user concern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
 
     def create
         user = User
@@ -15,6 +16,24 @@ class SessionsController < ApplicationController
         else
             render json: { status: 401 }
         end
+    end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def logout
+        reset_session
+        render json: { status: 200, logged_out: true }
     end
 
 end
